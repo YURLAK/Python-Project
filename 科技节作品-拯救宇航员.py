ed29ba49751528d5bb613ba0d0a940f9a393cf66
Sunyuhao <77920708+yohosun@users.noreply.github.com>
2021-12-02 19:50:00 +0800

    Update 科技节作品-拯救宇航员.py
    加上是否碰到零件的X和Y值判断。

--- 科技节作品-拯救宇航员.py
+++ 科技节作品-拯救宇航员.py
@@ -1,6 +1,7 @@
 #import blocks
 import pygame
 import sys,time,random
+from colorama import Fore
 #init
 pygame.init()#init pygame
 pygame.display.init()#init display
@@ -20,7 +21,7 @@ text = font.render("拯救宇航员",True,(200,250,255))
 font2 = pygame.font.SysFont("simHei",35)
 text2 = font2.render("空格开始",True,(250,20,0),(0,0,0))
 pygame.display.flip()#update window
-screen.blit(text, (140,80))
+screen.blit(text, (120,80))
 screen.blit(text2,(150,250))
 pygame.display.flip()
 while yes_or_no_start == False:
@@ -43,19 +44,19 @@ while yes_or_no_start == False:
                 #plus the text
                 text_spaceman_back_earth = font3.render("宇航员正要返回地球", True, (250, 0, 0))
                 text_spaceman_back_earth2 = font4.render("可却发现自己遇到了重重困难...", True, (250, 0, 0))
-                text_spaceman_back_earth3 = font5.render("Level1",True,(250,0,0))
+                text_spaceman_back_earth3 = font5.render("Level1",True,(0,0,250))
                 text_spaceman_back_earth4 = font6.render("帮助宇航员找回丢失的1个零件,WASD控制",True,(250,0,0))
                 #blit it
                 screen.blit(text_spaceman_back_earth,(130,180))
                 pygame.display.flip()
                 time.sleep(2)
                 screen.fill('black')
-                screen.blit(text_spaceman_back_earth2,(130,180))
+                screen.blit(text_spaceman_back_earth2,(120,180))
                 pygame.display.flip()
                 time.sleep(2)
                 pygame.display.init()
                 screen.fill('black')
-                screen.blit(text_spaceman_back_earth3,(170,170))
+                screen.blit(text_spaceman_back_earth3,(185,185))
                 pygame.display.flip()
                 time.sleep(2)
                 pygame.display.init()
@@ -70,40 +71,43 @@ while yes_or_no_start == False:
                 spaceman2 = pygame.image.load('宇航员.png')
                 l1 = pygame.image.load('零件1.png')
                 #设置三个零件的坐标
-                l1x = random.randint(0,100)
-                l1y = random.randint(0,100)
-                x = 150
-                y = 150
+                l1x = random.randint(2,400)
+                l1y = random.randint(2,400)
+                x = 194
+                y = 186
                 screen.blit(spaceman1,(x,y))
                 pygame.display.flip()
+                print(Fore.RED+"检测雷达已开启...")
+                time.sleep(1)
                 while True:
-                    if y == l1y and x == l1x:
-                        screen.blit(l1,(l1x,l1y))
-                        print('Yes')
-                    screen.blit(l1,(l1x,l1y))
+                    Y = l1y - y
+                    X = l1x - x
+                    if X <= 2 and X >= -2 and Y <= 2 and Y >= -2:
+                        exit(0)
+                    print(Fore.BLUE+f"x坐标差{X}"," ",f"y坐标差{Y}")
+                    #screen.blit(l1,(l1x,l1y))
                     for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                             exit(0)
                         else:
                             key_list = pygame.key.get_pressed()
                             if key_list[pygame.K_w]:
-                                y -= 2
+                                y -= 1
                                 screen.blit(spaceman1,(x,y))
                                 pygame.display.flip()
                                 screen.fill('black')
                             elif key_list[pygame.K_s]:
-                                y += 2
+                                y += 1
                                 screen.blit(spaceman1,(x,y))
                                 pygame.display.flip()
                                 screen.fill('black')
                             elif key_list[pygame.K_a]:
-                                x -= 2
+                                x -= 1
                                 screen.blit(spaceman1,(x,y))
                                 pygame.display.flip()
                                 screen.fill('black')
                             elif key_list[pygame.K_d]:
-                                x += 2
+                                x += 1
                                 screen.blit(spaceman2,(x,y))
                                 pygame.display.flip()
                                 screen.fill('black')
-
